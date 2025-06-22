85ee04e88769f42b002a3a16b270e7480a88e784
Brandon Johnson <sndrec32exe@gmail.com>
2025-06-22 04:10:12 -0400

    Some little changes.

--- mxto/car_container_node.gd
+++ mxto/car_container_node.gd
@@ -3,12 +3,12 @@ class_name CarNodeContainer extends Node3D
 var num_cars := 1
 
 func instantiate_cars(definitions: Array):
-        for child in get_children():
-                child.queue_free()
-        num_cars = definitions.size()
-        for i in num_cars:
-                var new_car := preload("res://vehicle/visual_car.tscn").instantiate()
-                new_car.car_definition = definitions[i]
-                add_child(new_car)
-                if i == 0:
-                        new_car.car_camera.make_current()
+	for child in get_children():
+		child.queue_free()
+	num_cars = definitions.size()
+	for i in num_cars:
+		var new_car := preload("res://vehicle/visual_car.tscn").instantiate()
+		new_car.car_definition = definitions[i]
+		add_child(new_car)
+		if i == 0:
+			new_car.car_camera.make_current()
