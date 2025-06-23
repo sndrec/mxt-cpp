39274d7091c43ef8bc9aa4b9967e036d264b133b
Brandon Johnson <sndrec32exe@gmail.com>
2025-06-22 22:10:59 -0400

    fix indentation

--- mxto/car_container_node.gd
+++ mxto/car_container_node.gd
@@ -3,12 +3,12 @@ class_name CarNodeContainer extends Node3D
 var num_cars := 100
 
 func instantiate_cars(definitions: Array, local_index: int = 0):
-        for child in get_children():
-                child.queue_free()
-        num_cars = definitions.size()
-        for i in num_cars:
-                var new_car := preload("res://vehicle/visual_car.tscn").instantiate()
-                new_car.car_definition = definitions[i]
-                add_child(new_car)
-                if i == local_index:
-                        new_car.car_camera.make_current()
+				for child in get_children():
+								child.queue_free()
+				num_cars = definitions.size()
+				for i in num_cars:
+								var new_car := preload("res://vehicle/visual_car.tscn").instantiate()
+								new_car.car_definition = definitions[i]
+								add_child(new_car)
+								if i == local_index:
+												new_car.car_camera.make_current()
