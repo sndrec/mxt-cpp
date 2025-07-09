6c8ea14a773942ee52bfc110284ed47e625d7f97
Twilight <sndrec32exe@gmail.com>
2025-07-09 15:55:57 -0700

    Add TriggerCollider framework

---
+++ src/track/trigger_collider.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "godot_cpp/variant/transform3d.hpp"
+#include "godot_cpp/variant/vector3.hpp"
+
+class PhysicsCar;
+
+namespace TRIGGER_TYPE {
+    enum TYPE {
+        DASHPLATE = 0,
+        JUMPPLATE = 1,
+        MINE      = 2
+    };
+}
+
+class TriggerCollider {
+public:
+    TRIGGER_TYPE::TYPE type = TRIGGER_TYPE::DASHPLATE;
+    godot::Transform3D transform;
+    godot::Vector3 half_extents;
+    godot::Transform3D inv_transform;
+
+    virtual ~TriggerCollider() = default;
+    virtual void start_touch(PhysicsCar* car);
+    virtual void touch(PhysicsCar* car);
+    virtual void end_touch(PhysicsCar* car);
+};
+
+class Dashplate : public TriggerCollider {
+public:
+    Dashplate();
+    void start_touch(PhysicsCar* car) override;
+    void touch(PhysicsCar* car) override;
+    void end_touch(PhysicsCar* car) override;
+};
+
+class Jumpplate : public TriggerCollider {
+public:
+    Jumpplate();
+    void start_touch(PhysicsCar* car) override;
+    void touch(PhysicsCar* car) override;
+    void end_touch(PhysicsCar* car) override;
+};
+
+class Mine : public TriggerCollider {
+public:
+    Mine();
+    void start_touch(PhysicsCar* car) override;
+    void touch(PhysicsCar* car) override;
+    void end_touch(PhysicsCar* car) override;
+};
+
