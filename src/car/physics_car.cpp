5b5f257d31d6ce94cad1e0bdcfa4949fa83e2ab2
Brandon Johnson <sndrec32exe@gmail.com>
2025-11-13 12:53:39 -0500

    s-boost power

--- src/car/physics_car.cpp
+++ src/car/physics_car.cpp
@@ -1112,6 +1112,11 @@ float PhysicsCar::handle_machine_accel_and_boost(float neg_local_fwd_speed, floa
 
 	base_speed = new_base_speed;
 
+	if (sboostActive)
+	{
+		base_speed += 0.05f;
+	}
+
 	if (input_brake <= 0.0001f)
 		brake_timer = 0;
 	else if (brake_timer < 0x1e)
