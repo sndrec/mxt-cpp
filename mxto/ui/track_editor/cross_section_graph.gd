ea2ed81006e8082689a446a508c4fd6659675d14
Brandon Johnson <sndrec32exe@gmail.com>
2026-05-12 23:36:31 -0400

    Make cross section graph read-only

--- mxto/ui/track_editor/cross_section_graph.gd
+++ mxto/ui/track_editor/cross_section_graph.gd
@@ -70,19 +70,6 @@ func _ready() -> void:
 	if dock and !dock.update_track.is_connected(update_track_cross_sections):
 		dock.update_track.connect(update_track_cross_sections)
 
-func snap_point_visual(in_point : Vector2) -> Vector2:
-	var cp := get_current_path()
-	if !cp:
-		return Vector2.ZERO
-	var width := 358.0
-	var half_width := width * 0.5
-	in_point /= size
-	var t := Vector2(in_point.x * 2.0 - 1.0, get_cross_section_value())
-	t.x = -t.x
-	var t_values := PackedVector2Array([t])
-	var local_points : PackedVector3Array = _local_cross_section_points(cp, t_values)
-	return _flat_cross_section_point(local_points[0], half_width)
-
 func find_closest_poly_point(in_pos : Vector2) -> int:
 	var cp := get_current_path()
 	if !cp:
@@ -99,7 +86,6 @@ func _draw():
 	if !is_visible_in_tree():
 		return
 	var mouse_pos := get_local_mouse_position()
-	var oob := mouse_pos.x < 0.0 or mouse_pos.y < 0.0 or mouse_pos.x > size.x or mouse_pos.y > size.y
 	if !cached_poly_valid:
 		cached_poly_flat_points = _poly_flat_points(cp)
 		cached_poly_closest = _closest_poly_point_from_points(cached_poly_flat_points, mouse_pos)
@@ -111,80 +97,19 @@ func _draw():
 			use_color = Color.RED
 		draw_circle(flat_point, 4.0, use_color, true, -1.0, true)
 	
-	if !oob and Input.is_key_pressed(KEY_ALT):
-		var p := get_local_mouse_position()
-		draw_circle(snap_point_visual(p), 4.0, Color.GREEN, true, -1.0, true)
-var right_clicked := false
-var left_clicked := false
 
-var target_closest := 0
-var dragging := false
-
-var old_mouse_pos := Vector2.ZERO
-
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if !is_visible_in_tree():
 		return
 	var cp := get_current_path()
 	if !cp:
 		return
 	var mouse_pos := get_local_mouse_position()
-	var mouse_motion := old_mouse_pos - mouse_pos
-	old_mouse_pos = mouse_pos
+	var previous_closest := cached_poly_closest
 	var closest := find_closest_poly_point(mouse_pos)
 	var oob := mouse_pos.x < 0.0 or mouse_pos.y < 0.0 or mouse_pos.x > size.x or mouse_pos.y > size.y
-	if !oob:
+	if !oob and closest != previous_closest:
 		queue_redraw()
-	
-	var right_was_clicked := right_clicked
-	var left_was_clicked := left_clicked
-	
-	var rmb := Input.is_mouse_button_pressed(MOUSE_BUTTON_RIGHT)
-	var lmb := Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)
-	var alt := Input.is_key_pressed(KEY_ALT)
-	
-	if rmb:
-		right_clicked = true
-	else:
-		right_clicked = false
-	
-	if lmb:
-		left_clicked = true
-	else:
-		left_clicked = false
-	
-	var jpl := left_clicked and !left_was_clicked and !oob
-	var jpr := right_clicked and !right_was_clicked and !oob
-	var jrl := !left_clicked and left_was_clicked
-	var jrr := !right_clicked and right_was_clicked
-	
-	if jpr:
-		cp.horizontal_road_mesh_segments.remove_at(closest)
-		cached_poly_valid = false
-		cp._try_generate_mesh()
-		update_track_cross_sections()
-	
-	if jpl:
-		if alt:
-			var desired_point := mouse_pos / size
-			cp.horizontal_road_mesh_segments.append(clamp(1.0 - desired_point.x, 0, 1))
-			cp.horizontal_road_mesh_segments.sort()
-			cached_poly_valid = false
-			cp._try_generate_mesh()
-			update_track_cross_sections()
-		else:
-			target_closest = closest
-			dragging = true
-	
-	if jrl:
-		dragging = false
-		
-	if dragging:
-		cp.horizontal_road_mesh_segments[target_closest] = clampf(cp.horizontal_road_mesh_segments[target_closest] + mouse_motion.x / size.x, 0, 1)
-		cp.horizontal_road_mesh_segments.sort()
-		cached_poly_valid = false
-		cp._try_generate_mesh()
-		update_track_cross_sections()
 
 func update_track_cross_sections() -> void:
 	cached_poly_valid = false
