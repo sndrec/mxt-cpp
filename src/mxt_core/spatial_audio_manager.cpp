24244a6960629cd3dee87e5695dbb5a05ed92bd7
Brandon Johnson <sndrec32exe@gmail.com>
2026-06-02 20:39:45 -0400

    better audio

--- src/mxt_core/spatial_audio_manager.cpp
+++ src/mxt_core/spatial_audio_manager.cpp
@@ -1415,7 +1415,9 @@ void MxtSpatialAudioManager::update_vehicle_loop_audio(GameSim* sim, double delt
 		}
 
 		const bool engine_active = car_audible;
-		const uint8_t engine_target_speed_control = mxt_audio_gx_engine_base_speed_control(soa.base_speed[lane]);
+		const float engine_base_speed = (machine_state & MACHINESTATE::STARTINGCOUNTDOWN) != 0u ?
+			std::min(soa.base_speed[lane], 1.0f) : soa.base_speed[lane];
+		const uint8_t engine_target_speed_control = mxt_audio_gx_engine_base_speed_control(engine_base_speed);
 		if (!loop_state.gx_engine_speed_initialized) {
 			loop_state.gx_engine_speed_control = engine_target_speed_control;
 			loop_state.gx_engine_speed_initialized = true;
