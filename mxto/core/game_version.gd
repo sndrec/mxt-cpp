06fa94dc22ac77130a033779ab3d60e279ea0de1
Brandon Johnson <sndrec32exe@gmail.com>
2026-08-13 15:48:54 -0400

    yippee

--- mxto/core/game_version.gd
+++ mxto/core/game_version.gd
@@ -1,8 +1,8 @@
 class_name GameVersion extends RefCounted
 
 const MAJOR := 0
-const COMPATIBILITY := 1
-const PATCH := 43
+const COMPATIBILITY := 2
+const PATCH := 00
 
 static func number_string() -> String:
 	return "%d.%d.%d" % [MAJOR, COMPATIBILITY, PATCH]
